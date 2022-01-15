6d4163db88b61a31043ef3ade72eb83993ac87ff
Erna Lukac <e.lukac4@gmail.com>
2022-01-15 09:37:00 -0700

    got rows with time, note section, and basics of t=sqave button, function to change color for current time is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-let now = moment();
+const date = moment().format('dddd, MMM Do');
+
+// declares possible times as an array so that they can be looped through
+const workTime = [7,8,9,10,11,12,13,14,15,16,17,18];
+
+// this compares time to now and changes color for past/present
+const compareTime = function() {
+    currentTime = moment().format('H');
+    for (let i = 0; i < workTime.length; i++){
+        if (currentTime > workTime[i]) {
+            $(`#${workTime[i]}Note`).attr('class','col-8 past')
+        }else if (currentTime == workTime[i]) {
+            $(`#${workTime[i]}Note`).attr('class','col-8 present')
+        }
+    }
+}
+
+// this runs the function every second so the compareTime function can change colors as time changes 
+$(function() {
+    setInterval(compareTime,1000)
+});
+
